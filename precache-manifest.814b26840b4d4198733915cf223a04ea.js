890a52930c562a219d881de4857c570187736944
ting-hu <huxu@uwplatt.edu>
2022-11-27 12:16:11 -0600

    Updates

---
+++ precache-manifest.814b26840b4d4198733915cf223a04ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40980013d64277cb40828dabf09ff47a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa5d42ef724b76f03302",
+    "url": "./static/css/2.d8077e82.chunk.css"
+  },
+  {
+    "revision": "f211db725ffe1290c351",
+    "url": "./static/css/main.b07af9cd.chunk.css"
+  },
+  {
+    "revision": "aa5d42ef724b76f03302",
+    "url": "./static/js/2.d915ff65.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "./static/js/2.d915ff65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f211db725ffe1290c351",
+    "url": "./static/js/main.de2ce04a.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
